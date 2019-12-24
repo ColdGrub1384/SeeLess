1295230ef89aa212334a8a793e78860f4057df5c
Adrian Labbé <adrilabbelol@gmail.com>
2019-12-24 20:20:36 -0300

    Remove 'package' command (yeah, you like to reject apps Apple Reviewer)

---
+++ SeeLess/package.swift
@@ -0,0 +1,17 @@
+//
+//  package.swift
+//  SeeLess
+//
+//  Created by Adrian Labbé on 24-12-19.
+//  Copyright © 2019 Adrian Labbé. All rights reserved.
+//
+
+import Foundation
+
+/// The 'package' command.
+func packageMain(argc: Int, argv: [String], io: LTIO) -> Int32 {
+    
+    fputs("package is not supported by SeeLess.\n".cValue, io.stdout)
+    
+    return 0
+}
